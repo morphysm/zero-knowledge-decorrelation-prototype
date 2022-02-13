a16bd8c9faf322927595870f95939b924e2e6227
Matthew Love <matthew@floating.group>
2022-02-13 11:31:26 -0500

    Configure CDK application

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+
+from aws_cdk import core
+
+from python.cdk_constructs.stack import ZicoBadgesStack
+
+app = core.App()
+
+ZicoBadgesStack(
+    scope=app,
+    id="Badges",
+    env=core.Environment(account='920316314555', region='us-east-1'),
+)
+app.synth()
