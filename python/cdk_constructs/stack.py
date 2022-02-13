2d36207bc88fb46dc0b3389be01fad51744b15c4
Matthew Love <matthew@floating.group>
2022-02-13 11:30:53 -0500

    Add user pool and client for authentication

--- python/cdk_constructs/stack.py
+++ python/cdk_constructs/stack.py
@@ -1,15 +1,16 @@
 """Stack for Badges project."""
 from typing import Any
 
-import aws_cdk
-import constructs
+from aws_cdk import core
+from python.cdk_constructs.api import Api
+from python.cdk_constructs.authentication import Authentication
 
 
-class Stack(constructs.Construct):
+class ZicoBadgesStack(core.Stack):
     def __init__(
         self,
-        scope: constructs.Construct,
-        constuct_id: str,
+        scope: core.Construct,
+        id: str,
         **kwargs: Any,
     ) -> None:
         """Stack representing one instance of the Badge service.
@@ -17,6 +18,8 @@ class Stack(constructs.Construct):
         Currently contains:
             API stack containing a full rest API with lambda
                 handlers for all endpoints.
+            Authentication stack containing resources to authentice
+                into API using AWS Cognito.
 
         args:
             scope: construct that is scope for this stack. All CDK
@@ -24,6 +27,7 @@ class Stack(constructs.Construct):
             construct_id: ID.
             **kwargs: arbitrary arguments passed to superclass constructor.
         """
-        super().__init__(scope, construct_id, **kwargs)
+        super().__init__(scope=scope, id=id, **kwargs)
 
-        self.api = Api(scope=self)
+        authentication = Authentication(scope=self, id=f"{id}Auth")
+        Api(scope=self, id=f"{id}Api", user_pool=authentication.user_pool)
