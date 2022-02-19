80093778a521a4b647e912434f9d2390b2b0165a
Matthew Love <matthew@floating.group>
2022-02-19 15:05:12 -0500

    Run black on root of repository

--- setup.py
+++ setup.py
@@ -8,38 +8,27 @@ with open("README.md") as fp:
 setuptools.setup(
     name="ZicoBadges",
     version="0.0.1",
-
     description="ZicoBadges App",
     long_description=long_description,
     long_description_content_type="text/markdown",
-
     author="MatthewLove",
-
     package_dir={"": "ZicoBadges"},
     packages=setuptools.find_packages(where="ZicoBadges"),
-
     install_requires=[
         "aws-cdk.core==1.89.0",
     ],
-
     python_requires=">=3.6",
-
     classifiers=[
         "Development Status :: 4 - Beta",
-
         "Intended Audience :: Developers",
-
         "License :: OSI Approved :: Apache Software License",
-
         "Programming Language :: JavaScript",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-
         "Topic :: Software Development :: Code Generators",
         "Topic :: Utilities",
-
         "Typing :: Typed",
     ],
 )
