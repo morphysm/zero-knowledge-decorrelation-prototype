0dbd45ab465a1696feb6b23cf436c3fb071bdce4
enricobottazzi <85900164+enricobottazzi@users.noreply.github.com>
2022-02-19 12:21:51 +0100

    Update ConnectMM.tsx
    Fixed Connect with MM

--- frontend/src/components/ConnectMM.tsx
+++ frontend/src/components/ConnectMM.tsx
@@ -3,16 +3,13 @@ import { Button } from "react-bootstrap";
 
 export default function ConnectMM() {
     const { activateBrowserWallet, account } = useEthers();
-    const etherBalance = useEtherBalance(account);
 
     function handleConnectWallet() {
         activateBrowserWallet();
       }
 
     return account ? (
-        <p className='m-4'>
-            {etherBalance && etherBalance} ETH
-        </p>
+        <h1> {account} </h1>
     ) : (
         <Button className='m-4 bg-dark border border-dark' onClick={activateBrowserWallet}>Connect to a wallet</Button>
     );
