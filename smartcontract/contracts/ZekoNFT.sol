8597ff65b902f34348ab469bf939202b2827440b
enricobottazzi <85900164+enricobottazzi@users.noreply.github.com>
2022-02-21 11:44:47 +0100

    Updated Smartcontract + added Etherscan verification

--- smartcontract/contracts/ZekoNFT.sol
+++ smartcontract/contracts/ZekoNFT.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 // create IPFS function to issue the NFT 
@@ -21,17 +22,22 @@ contract ZekoNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+
     // The contract deployer becomes the admin of the contract 
     address public admin;
 
     // Add a mapping of "claimers" 
     // Add a mapping of "whitelisted addresses"  
-    // (daoId => serverId => array of addresses, to fix) 
-    mapping (uint => mapping (uint => address [])) claimers;
-    mapping (uint => mapping (uint => address [])) whiteList;
+    // (daoId => roleId => array of addresses) 
+    mapping (uint => mapping (uint => address [])) public claimers;
+    mapping (uint => mapping (uint => address [])) public whiteList;
 
-    // Add a mapping that gets you from NFT RoleId to array of tokenId
-    mapping (uint => uint []) tokenRoleToId;
+    // Add a mapping that gets you from daoId => RoleId => array of NFT tokenIds that shows if 
+    // it's transferable or not
+    mapping (uint => mapping (uint => uint [])) public tokenRoleToId;
+
+    // Add a mapping that gets you from NFT id to its trasnferability
+    mapping (uint => bool) isNftTransferable;
 
     constructor() ERC721("Zeko", "ZKO") {
         admin = msg.sender;
@@ -39,43 +45,51 @@ contract ZekoNFT is ERC721URIStorage {
 
     // it should only be called by the admin (Zeko or Dao)
     // mint NFT without actually assigning it to anyone
-    function mintARole (string memory tokenURI, uint roleId) public {
+    function mintARole (string memory tokenURI, uint _roleId, uint _daoId) public{
         _tokenIds.increment();
         uint newItemId = _tokenIds.current();
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
-        tokenRoleToId[roleId].push(newItemId);
+        tokenRoleToId[_daoId][_roleId].push(newItemId);
         require(msg.sender == admin);
     }
-
 
     // - Claim the NFT (add your address to a list) (address => Dao Id => roleId)
-    function claimNft(uint daoId, uint roleId) public {
-        claimers[daoId][roleId].push(msg.sender);
+    function claimNft(uint _daoId, uint _roleId) public {
+        claimers[_daoId][_roleId].push(msg.sender);
     }
 
-
-    // - Add address to the white addresses (address => serverId => roleId) (Admin permission) after internal verification
-    function addToWhitelist (address verifiedClaimer, uint daoId, uint roleId) public {
-        whiteList[daoId][roleId].push(verifiedClaimer);
+    // - Add address to the white list (serverId => roleId => address) (Admin permission) after internal verification
+    function addToWhitelist (address verifiedClaimer, uint _daoId, uint _roleId) public {
+        whiteList[_daoId][_roleId].push(verifiedClaimer);
         require(msg.sender == admin);
     }
 
+    // - when called addresses inside the whitelist gets their NFT airdropped otherwise it gets burned
+    function blukAirdropForRole (uint _daoId, uint _roleId) public {
 
-    // - After the claiming time has experied airdrop, addresses inside the whitelist gets their NFT otherwise it gets burned
-    function blukAirdropForRole (uint serverId, uint roleId) public {
-
-        address [] memory addressesToTransfer = whiteList[serverId][roleId];
+        address [] memory addressesToTransfer = whiteList[_daoId][_roleId];
     
-        uint [] memory nftMintedIds = tokenRoleToId[roleId];
+        uint [] memory nftMintedIds = tokenRoleToId[_daoId][_roleId];
 
         for (uint i = 0; i < nftMintedIds.length; i++) {
-
             _transfer(msg.sender, addressesToTransfer[i], nftMintedIds[i]);
+            isNftTransferable[nftMintedIds[i]] = false;
         }
+
         require(msg.sender == admin);
     }
 
+
+   function _transfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) override internal {
+
+    require(isNftTransferable[tokenId]=true);
+    super._transfer(from, to, tokenId); 
+}
 }
 
     
--- smartcontract/hardhat.config.js
+++ smartcontract/hardhat.config.js
@@ -1,5 +1,6 @@
 require('@nomiclabs/hardhat-waffle');
 require('dotenv').config();
+require("@nomiclabs/hardhat-etherscan");
 
 module.exports = {
   solidity: "0.8.1",
@@ -7,8 +8,13 @@ module.exports = {
     matic: {
       url: `${process.env.ALCHEMY_MUMBAI_URL}`,
       accounts: [`0x${process.env.MUMBAI_PRIVATE_KEY}`],
-    } 
-  }
+    }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: 'M7YECU3EUF9X3K53HN8I62QUFNIUTDTC1Z'
+  }  
 };
 
 
