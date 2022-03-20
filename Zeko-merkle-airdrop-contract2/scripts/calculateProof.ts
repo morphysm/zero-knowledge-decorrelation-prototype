925b3e337b7586a4f0e8980a0654966395234c23
enricobottazzi <85900164+enricobottazzi@users.noreply.github.com>
2022-03-20 19:06:01 +0000

    Update calculateProof and claimButton scripts

--- Zeko-merkle-airdrop-contract2/scripts/calculateProof.ts
+++ Zeko-merkle-airdrop-contract2/scripts/calculateProof.ts
@@ -6,16 +6,17 @@ import { providers} from 'ethers';
 
 
 /** Collect an airdrop from the local merkle tree against deployed contract. */
-async function main() {
 
-    let WASM_PATH = "./build/circuit_js/circuit.wasm"; // THIS FILE SHOULD BE STORED IN THE PUBLIC FOLDER ON THE FRONTEND
-    let ZKEY_PATH = "./build/circuit_final.zkey"; // THIS FILE SHOULD BE STORED IN THE PUBLIC FOLDER ON THE FRONTEND
+//      let WASM_PATH = "./build/circuit_js/circuit.wasm"; // THIS FILE SHOULD BE STORED IN THE PUBLIC FOLDER ON THE FRONTEND
+//      let ZKEY_PATH = "./build/circuit_final.zkey"; // THIS FILE SHOULD BE STORED IN THE PUBLIC FOLDER ON THE FRONTEND
+// let commitmentsFileName = "./test/temp/publicCommitments.txt" // RETRIEVE IT FROM THE PUBLIC FOLDER STORED ON THE FRONTEND
+
+async function main(WASM_PATH : string, ZKEY_PATH: string, commitmentsFileName: string) : Promise<string> {
 
     let WASM_BUFF = readFileSync(WASM_PATH);
     let ZKEY_BUFF = readFileSync(ZKEY_PATH);
     
-    let commitmentsFileName = "./test/temp/publicCommitments.txt" // RETRIEVE IT FROM THE PUBLIC FOLDER STORED ON THE FRONTEND
-
+    
     // IN ORDER TO CONNECT THE WALLET ON THE FRONTEND
     // GET SIGNER
     let provider = new providers.Web3Provider(window.ethereum);
@@ -23,8 +24,8 @@ async function main() {
     let signer = provider.getSigner();
     let address = await signer.getAddress();
 
-    let keyHex  // RETRIEVE IT FROM THE STATE
-    let secretHex // RETRIEVE IT FROM THE STATE
+    let keyHex  // RETRIEVE IT FROM THE STATE SET PREVIOUSLY
+    let secretHex // RETRIEVE IT FROM THE STATE SET PREVIOUSLY
 
     let key = BigInt(keyHex)
     let secret = BigInt (secretHex)
@@ -38,14 +39,7 @@ async function main() {
             address,
             WASM_BUFF,
             ZKEY_BUFF);
-    console.log("Proof: ", proof); // SAVE IT IN THE STATE 
-
-    let keyHash = toHex(pedersenHash(key)); // SAVE IT IN THE STATE
-
+    console.log("Proof: ", proof); 
+    let keyHash = toHex(pedersenHash(key));
+    return proof // SAVE IT IN THE STATE
 }
-
-main().then(() => process.exit(0))
-    .catch(e => {
-        console.error(e);
-        process.exit(-1);
-    })
--- Zeko-merkle-airdrop-contract2/scripts/claimButton.ts
+++ Zeko-merkle-airdrop-contract2/scripts/claimButton.ts
@@ -5,12 +5,16 @@ import {toHex, pedersenHashConcat } from "zkp-merkle-airdrop-lib";
  * when a new commitment comes it, update the public list of commitments and the merkle root stored inside the airdrop contract 
  */
 
-async function main() {
 
-    let inputFileName = "./test/temp/publicCommitments.txt" // THIS FILE SHOULD BE STORED IN THE PUBLIC FOLDER ON THE FRONTEND
+// WHERE SHOULD THE INPUT COME FROM?
+
+ // let inputFileName = "./test/temp/publicCommitments.txt" RETRIEVE IT FROM THE PUBLIC FOLDER STORED ON THE FRONTEND
+
+async function main(inputFileName:string) : Promise<{key: string, secret: string}>  {
+
     let treeHeight = 5;
-    let keyHex = toHex(randomBigInt(31)) // SAVE IT AS STATE ON THE FRONTEND
-    let secretHex = toHex(randomBigInt(31)) // SAVE IT AS STATE ON THE FRONTEND
+    let keyHex = toHex(randomBigInt(31)) 
+    let secretHex = toHex(randomBigInt(31))
     let key = BigInt(keyHex)
     let secret = BigInt (secretHex)
     let commitment = pedersenHashConcat(key, secret)
@@ -18,11 +22,5 @@ async function main() {
     // update the public list of commitments
     addNewCommitment(inputFileName,hexCommitment,treeHeight)
     console.log(`new commitment generated ${hexCommitment} from key: ${keyHex} and secret ${secretHex}`)
+    return {key: keyHex, secret: secretHex} // SET THESE AS STATE, WE'LL NEED THAT LATER
 }
-
-main()
-    .then(() => process.exit(0))
-    .catch(e => {
-        console.error(e);
-        process.exit(1);
-    })
