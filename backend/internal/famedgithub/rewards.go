2232b5330a22db8747f0527490ae479dad0d6c4f
Kilian Käslin <kilian.kaeslin@googlemail.com>
2022-08-09 12:45:31 +0200

    fix: famed-github-backend urls

--- backend/internal/famedgithub/rewards.go
+++ backend/internal/famedgithub/rewards.go
@@ -9,13 +9,13 @@ import (
 // GetRewardsByOwner returns a list of rewards for a given owner as bytes.
 func (c *client) GetRewardsByOwner(ctx context.Context, owner string) ([]byte, error) {
 	var bytes []byte
-	_, err := c.execute(ctx, http.MethodGet, fmt.Sprintf("owners/%s/rewards", owner), nil, &bytes)
+	_, err := c.execute(ctx, http.MethodGet, fmt.Sprintf("/famed/owners/%s/rewards", owner), nil, &bytes)
 	return bytes, err
 }
 
 // GetRewardsByContributor returns a list of rewards for a given contributor as bytes.
 func (c *client) GetRewardsByContributor(ctx context.Context, contributor string) ([]byte, error) {
 	var bytes []byte
-	_, err := c.execute(ctx, http.MethodGet, fmt.Sprintf("contributors/%s/rewards", contributor), nil, &bytes)
+	_, err := c.execute(ctx, http.MethodGet, fmt.Sprintf("/famed/contributors/%s/rewards", contributor), nil, &bytes)
 	return bytes, err
 }
